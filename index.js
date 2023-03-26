923e4904057c74a54948272316ba2175f9e06564
SaNjaYsinGh1123 <sanjayfbd93544@gmail.com>
2023-03-26 17:53:24 +0530

    assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use('/',require('./routes'))
+
+
+
+app.listen(process.env.PORT ||8000, function(err){
+    if(err){
+        console.log('error:',err);
+    }
+   console.log('server is running on :');
+});
